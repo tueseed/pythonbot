06cf9a5dcb5cb1fcefb023c0cdbadc643f23fead
tueseed <tue_seed@hotmail.com>
2018-04-05 21:33:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'นี่คือหน้วเวบ python ที่รันบน heroku นะครับน้องมีน แต่เอา url ไปใส่ใน webhook ของ ไลน์ บอท แล้ว มัน verify ไม่ผ่านอ่ะ .'
+@app.route('/test')
+def nutt():
+    return 'ออมันเป็นอย่างนี้นี่เองหราาา'
 @app.route('/bot', methods=['POST'])
 
 def bot():
