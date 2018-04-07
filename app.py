e05f7ad7ca90b3da41a998f5e29e46dfa4b96ac0
tueseed <tue_seed@hotmail.com>
2018-04-07 16:36:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,9 +22,8 @@ def index():
     cursor = con.cursor()
     query ="SELECT * FROM tbl_holiday"
     cursor.execute(query)
-    for result in cursor.fetchall():
-        date = result[1]
-        return str(date)
+    result = cursor.fetchall()
+    return str(result)
     cursor.close()
     con.close()
     
