216266caade60ea1a9d68e59845758b8ac27df86
tueseed <tue_seed@hotmail.com>
2018-04-07 16:33:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def index():
     cursor.execute(query)
     for result in cursor.fetchall():
         date = result[1]
-        return date
+        return str(date)
     cursor.close()
     con.close()
     
