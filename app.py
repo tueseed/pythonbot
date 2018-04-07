77470b960867d3a8d67aabee2b4c5f991e864c76
tueseed <tue_seed@hotmail.com>
2018-04-07 09:05:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ def index():
     return 'นี่คือหน้วเวบ python ที่รันบน heroku นะครับน้องมีน แต่เอา url ไปใส่ใน webhook ของ ไลน์ บอท แล้ว มัน verify ไม่ผ่านอ่ะ .'
 @app.route('/test')
 def test():
-    return 'ทดสอบการสร้าง route'
+    a = input("please insert number...")
+    return a
  
  
 @app.route('/bot', methods=['POST'])
