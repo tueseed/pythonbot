3ae7f8449a8fd4e6a1bede6a6f3983562d7bec36
tueseed <tue_seed@hotmail.com>
2018-10-05 22:44:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def login_bud():
     content = request.get_json()
     contents = json.dumps(content)
     #a1 = contents['value1']
-    return "เย้ ๆ ๆ ๆ ๆ"
+    return contents[0]
  
 def reply(replyToken, textList):
     # Method สำหรับตอบกลับข้อความประเภท text กลับครับ เขียนแบบนี้เลยก็ได้ครับ
